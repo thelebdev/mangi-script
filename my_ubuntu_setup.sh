b3b1d2f2baf0d5c90083d1e6f934c5ad4c91c360
Sojharo <sojharo@live.com>
2016-04-28 14:57:29 +0500

    kerberos

--- my_ubuntu_setup.sh
+++ my_ubuntu_setup.sh
@@ -56,6 +56,7 @@ sudo npm install -g generator-angular-fullstack
 # Vim, Curl, Python - Some random useful stuff
 sudo apt-get install vim curl python-software-properties
 sudo apt-get install python-dev
+sudo apt-get install libkrb5-dev
 
 # Installing JDK and JRE
 sudo apt-get install default-jre
