1defcdf374accbb9de5ff01be2ab2896081db2d4
tchicotti <tchicotti@localhost.localdomain>
2017-11-22 10:03:11 -0200

    Added a few new packages like xclip, php7.1 and set in the end a upgrade and update to apt-get

--- my_ubuntu_setup.sh
+++ my_ubuntu_setup.sh
@@ -14,7 +14,7 @@
 sudo apt-get update
 
 # Installing build essentials
-sudo apt-get install build-essential libssl-dev
+sudo apt-get install -y build-essential libssl-dev python-software-properties
 
 # Nodejs and NVM
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
@@ -36,7 +36,7 @@ sudo npm install forever -g
 
 # Git - a version control system
 sudo apt-get update
-sudo apt-get install git
+sudo apt-get install -y git xclip
 
 # Grunt - an automated task runner
 sudo npm install -g grunt-cli
@@ -50,68 +50,81 @@ sudo npm install -g yo
 # Gulp - an automated task runner
 sudo npm install -g gulp-cli
 
+# Ionic and Cordova - hybrid mobile development
+sudo npm install -g gulp-cli
+
 # Angular FullStack - My favorite MEAN boilerplate (MEAN = MongoDB, Express, Angularjs, Nodejs)
 sudo npm install -g generator-angular-fullstack
 
 # Vim, Curl, Python - Some random useful stuff
-sudo apt-get install vim curl python-software-properties
-sudo apt-get install python-dev, python-pip
-sudo apt-get install libkrb5-dev
+sudo apt-get install -y vim curl python-software-properties
+sudo apt-get install -y python-dev, python-pip
+sudo apt-get install -y libkrb5-dev
 
 # Installing JDK and JRE
-sudo apt-get install default-jre
-sudo apt-get install default-jdk
+sudo apt-get install -y default-jre
+sudo apt-get install -y default-jdk
 
 # Gimp Image Editor
-sudo apt-get install gimp gimp-data gimp-plugin-registry gimp-data-extras
+sudo apt-get install -y gimp gimp-data gimp-plugin-registry gimp-data-extras
 
 # Archive Extractors
-sudo apt-get install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller
+sudo apt-get install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller
+
+# PHP and Composer
+sudo apt-get install -y language-pack-en-base
+sudo LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install -y nginx mcrypt
+sudo apt-get install -y php7.1 php7.1-xml php7.1-mbstring php7.1-mysql php7.1-json php7.1-curl php7.1-cli php7.1-common php7.1-mcrypt php7.1-gd libapache2-mod-php7.1 php7.1-zip
+sudo service php7.1-fpm restart
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
 
 # FileZilla - a FTP client
-sudo apt-get install filezilla
+sudo apt-get install -y filezilla
 
 # TLP - saves battery when Ubuntu is installed on Laptops
 sudo apt-get remove laptop-mode-tools
 sudo add-apt-repository ppa:linrunner/tlp
 sudo apt-get update
-sudo apt-get install tlp tlp-rdw smartmontools ethtool
+sudo apt-get install -y tlp tlp-rdw smartmontools ethtool
 sudo tlp start
 sudo tlp stat
 
 # Erlang - Actor Concurrency Model based Programming Language
-sudo apt-get install erlang
+sudo apt-get install -y erlang
 
 # Sticky Notes
-sudo apt-get install xpad
+sudo apt-get install -y xpad
 
 # KVM acceleration and cpu checker
-sudo apt-get install cpu-checker
-sudo apt-get install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils
-sudo apt-get install virt-manager
-sudo apt-get install lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
+sudo apt-get install -y cpu-checker
+sudo apt-get install -y qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils
+sudo apt-get install -y virt-manager
+sudo apt-get install -y lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
 
 # RPM and alien - sometimes used to install software packages
-sudo apt-get install rpm
-sudo apt-get install alien dpkg-dev debhelper
+sudo apt-get install -y rpm
+sudo apt-get install -y alien dpkg-dev debhelper
 
 # Calibre - Ebook reader and converter
 sudo -v && wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
 
+# Zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # Dictionary Client and Server with Thesaurus
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
-sudo apt-get install dict
-sudo apt-get install dictd
-sudo apt-get install dict-gcide
-sudo apt-get install dict-wn
-sudo apt-get install dict-devil
-sudo apt-get install dict-moby-thesaurus
+sudo apt-get install -y dict
+sudo apt-get install -y dictd
+sudo apt-get install -y dict-gcide
+sudo apt-get install -y dict-wn
+sudo apt-get install -y dict-devil
+sudo apt-get install -y dict-moby-thesaurus
 
 # For Android Studio (Gradle Daemon)
 #touch ~/.gradle/gradle.properties && echo "org.gradle.daemon=true" >> ~/.gradle/gradle.properties
-
-# Zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Docker
 curl -fsSL get.docker.com -o get-docker.sh
@@ -124,3 +137,7 @@ sudo dpkg -i atom-amd64.deb
 sudo apt-get -f install
 # Nuclide
 apm install nuclide
+
+# Apt Upgrade and Update
+sudo apt-get upgrade -y 
+sudo apt-get update
