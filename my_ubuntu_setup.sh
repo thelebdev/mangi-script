497a6359ed56902f5a5726b6d0397c13e7294c18
Sojharo <sojharo@live.com>
2018-03-23 18:02:10 +0500

    automatically approve on installation

--- my_ubuntu_setup.sh
+++ my_ubuntu_setup.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-#Sojharo's Development Machine Setup on Ubuntu
-#Author : Sojharo
+# Sojharo's Development Machine Setup on Ubuntu
+# Author : Sojharo
 #
 # set software sources to server for united states
-# VLC from software center
-# virtual box from software center
-# PDF Mod from software center
-# shutter from software center
-# kazam from software center
-# install unity tweak tool from software center
-# install rebar: pull from github (for building erlang applications)
+#
+# For other software:
+## VLC from software center
+## virtual box from software center
+## PDF Mod from software center
+## shutter from software center
+## kazam from software center
+## install unity tweak tool from software center
+## install rebar: pull from github (for building erlang applications)
 
 sudo apt-get update
 
 # Installing build essentials
-sudo apt-get install build-essential libssl-dev
+sudo apt-get install -y build-essential libssl-dev
 
 # Nodejs and NVM
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
@@ -40,7 +42,7 @@ sudo npm install forever -g
 
 # Git - a version control system
 sudo apt-get update
-sudo apt-get install git
+sudo apt-get install -y git xclip
 
 # Grunt - an automated task runner
 sudo npm install -g grunt-cli
@@ -62,58 +64,58 @@ sudo npm install -g gulp-cli
 sudo npm install -g generator-angular-fullstack
 
 # Vim, Curl, Python - Some random useful stuff
-sudo apt-get install vim curl python-software-properties
-sudo apt-get install python-dev, python-pip
-sudo apt-get install libkrb5-dev
+sudo apt-get install -y vim curl python-software-properties
+sudo apt-get install -y python-dev, python-pip
+sudo apt-get install -y libkrb5-dev
 
 # Installing JDK and JRE
-sudo apt-get install default-jre
-sudo apt-get install default-jdk
+sudo apt-get install -y default-jre
+sudo apt-get install -y default-jdk
 
 # Gimp Image Editor
-sudo apt-get install gimp gimp-data gimp-plugin-registry gimp-data-extras
+sudo apt-get install -y gimp gimp-data gimp-plugin-registry gimp-data-extras
 
 # Archive Extractors
-sudo apt-get install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller
+sudo apt-get install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller
 
 # FileZilla - a FTP client
-sudo apt-get install filezilla
+sudo apt-get install -y filezilla
 
 # TLP - saves battery when Ubuntu is installed on Laptops
 sudo apt-get remove laptop-mode-tools
 sudo add-apt-repository ppa:linrunner/tlp
 sudo apt-get update
-sudo apt-get install tlp tlp-rdw smartmontools ethtool
+sudo apt-get install -y tlp tlp-rdw smartmontools ethtool
 sudo tlp start
 sudo tlp stat
 
 # Erlang - Actor Concurrency Model based Programming Language
-sudo apt-get install erlang
+sudo apt-get install -y erlang
 
 # Sticky Notes
-sudo apt-get install xpad
+sudo apt-get install -y xpad
 
 # KVM acceleration and cpu checker
-sudo apt-get install cpu-checker
-sudo apt-get install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils
-sudo apt-get install virt-manager
-sudo apt-get install lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
+sudo apt-get install -y cpu-checker
+sudo apt-get install -y qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils
+sudo apt-get install -y virt-manager
+sudo apt-get install -y lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
 
 # RPM and alien - sometimes used to install software packages
-sudo apt-get install rpm
-sudo apt-get install alien dpkg-dev debhelper
+sudo apt-get install -y rpm
+sudo apt-get install -y alien dpkg-dev debhelper
 
 # Calibre - Ebook reader and converter
 sudo -v && wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
 
 # Dictionary Client and Server with Thesaurus
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
-sudo apt-get install dict
-sudo apt-get install dictd
-sudo apt-get install dict-gcide
-sudo apt-get install dict-wn
-sudo apt-get install dict-devil
-sudo apt-get install dict-moby-thesaurus
+sudo apt-get install -y dict
+sudo apt-get install -y dictd
+sudo apt-get install -y dict-gcide
+sudo apt-get install -y dict-wn
+sudo apt-get install -y dict-devil
+sudo apt-get install -y dict-moby-thesaurus
 
 # For Android Studio (Gradle Daemon)
 #touch ~/.gradle/gradle.properties && echo "org.gradle.daemon=true" >> ~/.gradle/gradle.properties
@@ -132,3 +134,5 @@ sudo dpkg -i atom-amd64.deb
 sudo apt-get -f install
 # Nuclide
 apm install nuclide
+
+sudo apt-get upgrade -y
